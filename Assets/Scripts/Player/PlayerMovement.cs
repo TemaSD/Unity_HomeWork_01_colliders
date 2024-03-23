0c141e8ae425c6020a8116080115aab3eecb67d2
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-03-23 18:51:09 +0200

    Added Colision message

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody collideWithEnemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Triggered with: {other.gameObject}");
+    }
 
     void Update()
     {
@@ -13,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(-1*moveSpeed, 0, 0);
+            transform.Translate(-moveSpeed, 0, 0);
         if (Input.GetKey(KeyCode.D))
             transform.Translate(moveSpeed, 0, 0);
     }
